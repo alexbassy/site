9311142977e725f766df64956ee80dac54b713e6
Alex Bass <alexpjbass@gmail.com>
2018-11-23 07:41:06 +0100

    add in progress header scroll animation

---
+++ pages/playground/header-scroll-animation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>iOS/Spotify style header animation / Alex Bass</title>
+    </Head>
+    <div>
+      <h1>
+        In progress
+      </h1>
+    </div>
+  </div>
+)
