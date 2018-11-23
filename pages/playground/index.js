e225f1bf2bec9bfcdd015dd4fc8b584d0b91be9b
Alex Bass <alexpjbass@gmail.com>
2018-11-23 07:40:40 +0100

    seed playground with simple responsive image example

---
+++ pages/playground/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const links = [
+  {
+    icon: `📸`,
+    path: 'responsive-images',
+    label: 'HTML/CSS only responsive, HiDPI friendly images with `srcset` and `sizes` attributes'
+  },
+  {
+    icon: `🍿`,
+    path: 'header-scroll-animation',
+    label: 'iOS header scrolling animation, and header item staggered animation ala Spotify'
+  }
+]
+
+const ListItem = ({ icon, path, label }) => (
+  <li>
+    <style jsx>{`
+    li {
+      position: relative;
+      max-width: 50ch;
+      font-size: 1.2rem;
+      margin-bottom: 1rem;
+      list-style: none;
+    }
+    li:before {
+      content: '${icon}';
+      position: absolute;
+      margin-left: -2rem;
+    }
+  `}</style>
+    <Link href={`/playground/${path}`}>
+      <a>{label}</a>
+    </Link>
+  </li>
+)
+
+export default () => (
+  <div>
+    <Head>
+      <title>Playground / Alex Bass</title>
+    </Head>
+    <style jsx>{`
+      main {
+        margin: 0 2rem;
+      }
+    `}</style>
+    <main>
+      <h1>
+        Stuff
+      </h1>
+      <ul>
+        {links.map(link =>
+          <ListItem key={link.path} {...link} />)}
+      </ul>
+    </main>
+  </div>
+)
---
+++ pages/playground/responsive-images.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+  <main>
+    <Head>
+      <title>iOS/Spotify style header animation / Alex Bass</title>
+    </Head>
+    <style jsx>{`
+      main {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+      img {
+        width: 100px;
+        height: 100px;
+      }
+      @media (min-width: 768px) {
+        img {
+          width: 200px;
+          height: 200px;
+        }
+      }
+      @media (min-width: 1200px) {
+        img {
+          width: 300px;
+          height: 300px;
+        }
+      }
+      @media (max-height: 300px) {
+        img {
+          width: 200px;
+          height: 200px;
+        }
+      }
+      p {
+        font-size: 14px;
+        color: #00000070;
+        max-width: 50ch;
+        text-align: center;
+      }
+    `}</style>
+    <img
+      src="https://via.placeholder.com/100"
+
+      srcSet=" https://via.placeholder.com/100 100w,
+        https://via.placeholder.com/200 200w,
+        https://via.placeholder.com/400 400w,
+        https://via.placeholder.com/600 600w"
+
+        sizes="(min-width: 1200px) 300px,
+               (min-width: 768px)  200px,
+               100px"
+    />
+    <div>
+      <p>To see images change, open inspector, disable cache, and resize the page</p>
+    </div>
+  </main>
+)
