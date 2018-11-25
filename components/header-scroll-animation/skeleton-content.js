bab8eb28f16e3128ff31b87949e11838ff833e33
Alex Bass <alexpjbass@gmail.com>
2018-11-25 08:47:31 +0100

    optimise skeleton content component

--- components/header-scroll-animation/skeleton-content.js
+++ components/header-scroll-animation/skeleton-content.js
@@ -8,48 +8,53 @@ const SkeletonContentItem = (props) => {
       <div data-id='fakename'></div>
       <div data-id='fakealbum'></div>
       <style jsx>{`
-      li {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        height: 50px;
-        border-bottom: 1px solid #ffffff20;
-        padding: 0 ${PAGE_PADDING}px;
-      }
-      [data-id=avatar] {
-        flex: 0 0 24px;
-        width: 24px;
-        height: 24px;
-        border-radius: 32px;
-        margin-right: 20px;
-        background: #ffffff20;
-      }
-      [data-id=fakename],
-      [data-id=fakealbum] {
-        flex: 1;
-        background: #ffffff20;
-        height: 14px;
-      }
-      [data-id=fakename] {
-        margin-right: 20px;
-      }
+        li {
+          display: flex;
+          flex-wrap: wrap;
+          align-items: center;
+          height: 50px;
+          border-bottom: 1px solid #ffffff20;
+          padding: 0 ${PAGE_PADDING}px;
+        }
+        [data-id=avatar] {
+          flex: 0 0 24px;
+          width: 24px;
+          height: 24px;
+          border-radius: 32px;
+          margin-right: 20px;
+          background: #ffffff20;
+        }
+        [data-id=fakename],
+        [data-id=fakealbum] {
+          flex: 1;
+          background: #ffffff20;
+          height: 14px;
+        }
+        [data-id=fakename] {
+          margin-right: 20px;
+        }
       `}</style>
     </li>
   )
 }
 
-const SkeletonContent = (props) => {
-  return (
-    <div>
-      <ul>
-        {[...Array(50).keys()].map(k =>
-          <SkeletonContentItem key={k} />)}
-      </ul>
-      <style jsx>{`
-        ul {padding: 0}
-      `}</style>
-    </div>
-  )
+class SkeletonContent extends React.PureComponent {
+  // completely static, never rerender
+  shouldComponentUpdate = () => false
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {[...Array(50).keys()].map(k =>
+            <SkeletonContentItem key={k} />)}
+        </ul>
+        <style jsx>{`
+          ul {padding: 0}
+        `}</style>
+      </div>
+    )
+  }
 }
 
 export default SkeletonContent
