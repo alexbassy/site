a06f8c907c4078cbcb18dd30c936587acedb4f8a
Alex Bass <alexpjbass@gmail.com>
2018-12-03 08:59:06 +0100

    use @emotion/styled where possible, add canvas page

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { Global, css } from '@emotion/core'
 import getConfig from 'next/config'
 
 const analyticsSnippet = `
@@ -7,6 +8,39 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-129387017-1');
 `.trim()
+
+const globalCSS = css`
+:root {
+  --text: hsl(56, 100%, 15%);
+  --bg: hsl(56, 100%, 50%);
+  --links: hsl(30, 100%, 33%);
+  --margin: 20px;
+}
+*, *:before, *:after {
+  box-sizing: border-box;
+}
+html {
+  height: 100%;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: 'Roboto Mono', monospace;
+}
+#__next {
+  height: 100%;
+}
+a, a:visited {
+  color: var(--links);
+  font-weight: 700;
+}
+a:hover {
+  color: var(--links);
+}
+`
 
 export default class MyDocument extends Document {
   static async getInitialProps (ctx) {
@@ -24,40 +58,9 @@ export default class MyDocument extends Document {
           <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,700' rel='stylesheet' />
           <script async src='https://www.googletagmanager.com/gtag/js?id=UA-129387017-1' />
           <script id='ga' dangerouslySetInnerHTML={{ __html: analyticsSnippet }} />
-          <style global jsx>{`
-          :root {
-            --text: hsl(56, 100%, 15%);
-            --bg: hsl(56, 100%, 50%);
-            --links: hsl(30, 100%, 33%);
-            --margin: 20px;
-          }
-          *, *:before, *:after {
-            box-sizing: border-box;
-          }
-          html {
-            height: 100%;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-          }
-          body {
-            margin: 0;
-            padding: 0;
-            height: 100%;
-            font-family: 'Roboto Mono', monospace;
-          }
-          #__next {
-            height: 100%;
-          }
-          a, a:visited {
-            color: var(--links);
-            font-weight: 700;
-          }
-          a:hover {
-            color: var(--links);
-          }
-          `}</style>
         </Head>
         <body>
+          <Global styles={globalCSS} />
           <Main className='main' />
           <NextScript />
           <script async src='https://www.googletagmanager.com/gtag/js?id=UA-129387017-1' />
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,47 @@
 import React from 'react'
 import Head from 'next/head'
-import Hello from '../components/hello.js'
-import Links from '../components/links.js'
+import { Global, css } from '@emotion/core'
+import styled from '@emotion/styled'
+import CanvasLib from '../lib/canvas'
 
-export default () => (
-  <div>
-    <Head>
-      <title>Alex Bass / Front-end developer</title>
-      <meta name='description' content='Front-end developer from Devon, UK, based in Barcelona'/>
-    </Head>
-    <style global jsx>{`
-      body {
-        display: flex;
-        flex-direction: column;
-        color: var(--text);
-        background-color: var(--bg);
-      }
-      #__next {
-        display: flex;
-        flex-direction: column;
-        flex: 1;
-      }
-    `}</style>
-    <style jsx>{`
-      div {
-        display: flex;
-        flex-direction: column;
-        flex: 1;
-      }
-    `}</style>
-    <Hello />
-    <Links />
-  </div>
-)
+const Canvas = styled.canvas`
+  width: 100%;
+  height: 100%;
+`
+
+const Container = styled.div`
+  height: 100%;
+`
+
+class Index extends React.Component {
+  constructor (props) {
+    super(props)
+    this.canvasRef = React.createRef()
+  }
+
+  componentDidMount () {
+    const controlled = new CanvasLib(this.canvasRef.current)
+    this.setState({
+      canvas: controlled
+    })
+  }
+
+  render () {
+    return (
+      <Container>
+        <Head>
+          <title>Alex Bass / Front-end developer</title>
+          <meta name='description' content='Front-end developer from Devon, UK, based in Barcelona' />
+        </Head>
+        <Global
+          style={css`
+
+          `}
+        />
+        <Canvas ref={this.canvasRef} />
+      </Container>
+    )
+  }
+}
+
+export default Index
