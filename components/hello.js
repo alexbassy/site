36013a10dd9dc6ae83a874af9ed8cc4688fb153e
Alex Bass <alexpjbass@gmail.com>
2018-12-03 22:38:12 +0100

    responsive improvements

--- components/hello.js
+++ components/hello.js
@@ -5,14 +5,21 @@ const Typeform = () => <a href='https://www.typeform.com/' target='_blank' rel='
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  text-align: center;
+
+  @media screen and (min-width: 640px) {
+    text-align: left;
+  }
 `
 
 const Title = styled.h1`
-  font-size: 3rem;
-  margin: 0;
+  font-size: 3.2rem;
+  margin: 0 0 25px;
+  font-weight: 100;
 
   @media screen and (min-width: 640px) {
     font-size: 4.5rem;
+    margin: 0 0 10px;
   }
 `
 
--- components/links.js
+++ components/links.js
@@ -2,22 +2,42 @@ import Link from 'next/link'
 import styled from '@emotion/styled'
 
 const List = styled.ul`
+  display: flex;
   list-style: none;
+  margin-top: 30px;
   padding-left: 0;
-  margin: var(--margin) 0;
+
+  @media screen and (max-width: 640px) {
+    flex-wrap: wrap;
+    align-items: stretch;
+    max-width: 270px;
+    margin: 15px auto 0;
+  }
 `
 
 const Item = styled.li`
   display: inline-block;
   margin-right: 1rem;
+
+  @media screen and (max-width: 640px) {
+    flex: 1 0 calc(50% - 15px);
+    margin: var(--margin) 0 0;
+
+    :nth-of-type(odd) {
+      margin-right: 15px;
+    }
+  }
 `
 
 const LinkButton = styled.a`
+  display: block;
+  text-align: center;
   font-weight: 100;
   text-decoration: none;
   color: #fff;
+  background: #111;
   border: 2px solid var(--linksDimmed);
-  padding: 2px 6px;
+  padding: 6px;
   transition: border-color .2s ease, color .15s ease;
 
   :visited {
@@ -30,6 +50,10 @@ const LinkButton = styled.a`
 
   :hover {
     border-color: #ffffffaa;
+  }
+
+  @media screen and (min-width: 640px) {
+    padding: 4px 8px;
   }
 `
 
