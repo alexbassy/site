0c66a49d14fe8b52704fc07cc76a16a9cffa1f6b
Alex Bass <alexpjbass@gmail.com>
2019-03-19 22:09:33 +0100

    add polygon lorem ipsum

---
+++ components/polygons/Polygon.js
@@ -0,0 +1,53 @@
+export default class Polygon {
+  constructor ({ vertices, size }) {
+    this.vertices = vertices
+    this.size = 100
+  }
+
+  getPoints () {
+    const points = []
+    const { vertices } = this
+    const size = this.size / 2
+    const center = this.size / 2
+
+    points.push([
+      center + size * Math.cos(0),
+      center + size * Math.sin(0)
+    ])
+
+    for (let i = 1; i < vertices; ++i) {
+      points.push([
+        center + size * Math.cos(i * 2 * Math.PI / vertices),
+        center + size * Math.sin(i * 2 * Math.PI / vertices)
+      ])
+    }
+
+    return points
+  }
+
+  getPointsAsClipPath () {
+    const points = this.getPoints()
+    const getPointAsPercentage = p => Math.floor(p) + '%'
+    return `polygon(${
+      points.map(xy => xy.map(getPointAsPercentage).join(' '))
+    })`
+  }
+
+  render (node) {
+    node.innerHTML = ''
+    const elem = document.createElement('div')
+
+    const points = this.getPoints()
+    const polygon = `polygon(${
+      points.map(xy => `${xy[0]}% ${xy[1]}%`)
+    })`
+    elem.style.clipPath = polygon
+    elem.style.shapeOutside = polygon
+
+    elem.style.width = `${this.size}px`
+    elem.style.height = `${this.size}px`
+    elem.style.backgroundColor = '#000'
+
+    node.appendChild(elem)
+  }
+}
---
+++ pages/playground/polygons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+import PolygonsExample from '../../components/polygons'
+
+export default () => (
+  <div>
+    <Head>
+      <title>Polygons with CSS, SVG and Canvas / Alex Bass</title>
+      <meta name='description' content='Showing the various ways to make shapes (polygons) with CSS, SVG and Canvas (JS)' />
+    </Head>
+    <PolygonsExample />
+  </div>
+)
