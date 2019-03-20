c7ae3852d237760029df087c4407754deddda26f
Alex Bass <alexpjbass@gmail.com>
2019-03-20 10:36:18 +0100

    separate routes for generator and wall of text

--- pages/playground/polygons.js
+++ pages/playground/polygons.js
@@ -2,12 +2,18 @@ import React from 'react'
 import Head from 'next/head'
 import PolygonsExample from '../../components/polygons'
 
-export default () => (
-  <div>
+const PolygonsPage = ({ query }) => (
+  <>
     <Head>
       <title>Polygons with CSS, SVG and Canvas / Alex Bass</title>
       <meta name='description' content='Showing the various ways to make shapes (polygons) with CSS, SVG and Canvas (JS)' />
     </Head>
-    <PolygonsExample />
-  </div>
+    <PolygonsExample view={query.view} />
+  </>
 )
+
+PolygonsPage.getInitialProps = async ({ query }) => {
+  return { query }
+}
+
+export default PolygonsPage
