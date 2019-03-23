0adb79406c3526e4f06345df606b4eb50cc903ac
Alex Bass <alexpjbass@gmail.com>
2019-03-23 19:07:56 +0100

    use functions instead of class

--- pages/playground/polygons.js
+++ pages/playground/polygons.js
@@ -2,18 +2,19 @@ import React from 'react'
 import Head from 'next/head'
 import PolygonsExample from '../../components/polygons'
 
-const PolygonsPage = ({ query }) => (
+const PolygonsPage = ({ view }) => (
   <>
     <Head>
       <title>Polygons with CSS, SVG and Canvas / Alex Bass</title>
       <meta name='description' content='Showing the various ways to make shapes (polygons) with CSS, SVG and Canvas (JS)' />
     </Head>
-    <PolygonsExample view={query.view} />
+    <PolygonsExample view={view} />
   </>
 )
 
 PolygonsPage.getInitialProps = async ({ query }) => {
-  return { query }
+  const view = query.view || 'generator'
+  return { view }
 }
 
 export default PolygonsPage
