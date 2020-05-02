068f8edfb0ecddac5cf77023c5ccf73de5cc55a9
Alex Bass <alexpjbass@gmail.com>
2020-05-02 17:26:01 +0200

    Add markdown dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
+const withMDX = require('@next/mdx')()
 const basePath = process.env.PREVIEW_PATH || ''
 const assetPrefix = process.env.PREVIEW_PATH || ''
 
-module.exports = {
+module.exports = withMDX({
   trailingSlash: true,
   basePath,
   assetPrefix,
   publicRuntimeConfig: {
     assetPrefix,
   },
-}
+})
