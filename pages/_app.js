35eb9878013ff5c9b7752de18e81ddf9a783d0ec
Alex Bass <alexpjbass@gmail.com>
2020-07-13 09:20:52 +0200

    add web vital reporting

--- pages/_app.js
+++ pages/_app.js
@@ -5,4 +5,14 @@ import * as gtag from '../lib/gtag'
 
 Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
+export function reportWebVitals({ id, name, label, value }) {
+  gtag.event({
+    action: name,
+    category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    label: id,
+    value: Math.round(name === 'CLS' ? value * 1000 : value),
+    nonInteraction: true, // avoids affecting bounce rate.
+  })
+}
+
 export default App
