6e75d4f41afedc5725feda6b6230da88c00d95e5
Alex Bass <alexpjbass@gmail.com>
2020-07-28 17:02:10 +0200

    both numerical

--- components/info-sheet/InfoSheet.js
+++ components/info-sheet/InfoSheet.js
@@ -9,8 +9,8 @@ import {
 } from './visual-components'
 
 const infoSheetStates = {
-  visible: { y: '0%', opacity: 1 },
-  hidden: { y: '100%', opacity: 0 },
+  visible: { y: 0, opacity: 1 },
+  hidden: { y: 300, opacity: 0 },
 }
 
 const infoSheetCurtainStates = {
