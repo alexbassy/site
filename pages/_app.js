ff76a1e91551a2dd125856789e61faf3775a58e9
Alex Bass <alexpjbass@gmail.com>
2020-10-11 15:46:24 +0200

    Remove google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,6 @@
 import React from 'react'
 import App from 'next/app'
 import Head from 'next/head'
-import Router from 'next/router'
-
-import * as gtag from '../lib/gtag'
-
-Router.events.on('routeChangeComplete', url => gtag.pageview(url))
-
-export function reportWebVitals({ id, name, label, value }) {
-  gtag.event({
-    action: name,
-    category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-    label: id,
-    value: Math.round(name === 'CLS' ? value * 1000 : value),
-    nonInteraction: true, // avoids affecting bounce rate.
-  })
-}
 
 function CustomApp({ Component, pageProps }) {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -2,13 +2,6 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { Global, css } from '@emotion/core'
 import getConfig from 'next/config'
-import { GA_TRACKING_ID } from '../lib/gtag'
-
-const analyticsSnippet =
-  `window.dataLayer = window.dataLayer || [];` +
-  `function gtag(){dataLayer.push(arguments);}` +
-  `gtag('js', new Date());` +
-  `gtag('config','${GA_TRACKING_ID}', { page_path: window.location.pathname });`
 
 const globalCSS = css`
   :root {
@@ -50,14 +43,6 @@ export default class Page extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            id='ga'
-            dangerouslySetInnerHTML={{ __html: analyticsSnippet }}
-          />
           <link
             rel='icon'
             href={`${publicRuntimeConfig.assetPrefix}/assets/favicon.png`}
