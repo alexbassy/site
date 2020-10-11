355a3bf7ccf4c9e0fbb16c378dd478a003a88d19
Alex Bass <alexpjbass@gmail.com>
2020-10-11 11:04:08 +0200

    Add `basePath`

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const basePath = process.env.PREVIEW_PATH || ''
 const assetPrefix = process.env.PREVIEW_PATH || ''
 
 module.exports = {
   trailingSlash: true,
+  basePath,
   assetPrefix,
   publicRuntimeConfig: {
     assetPrefix,
