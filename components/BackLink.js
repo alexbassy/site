482c485cd93c4b35926cabfdcc21fcc6bbc37972
Alex Bass <alexpjbass@gmail.com>
2020-11-18 10:02:47 +0100

    Back link styling, still not relative

--- components/BackLink.js
+++ components/BackLink.js
@@ -6,20 +6,22 @@ import { BACK_ARROW } from '../lib/constants'
 const Link = styled.a`
   text-decoration: none;
   color: ${props =>
-    props.invert ? `rgba(0 0 0 0.3)` : `rgba(255 255 255 0.3)`};
+    props.invert ? `rgb(0 0 0 / 0.8)` : `rgb(255 255 255 / 0.8)`};
   border-radius: 3px;
   padding: 2px;
   cursor: pointer;
 
   :hover {
     color: white;
+    color: ${props =>
+      props.invert ? `rgb(0 0 0 / 0.5)` : `rgb(255 255 255 / 0.5)`};
   }
 
   :focus {
     outline: none;
     transition: box-shadow 0.25s ease;
     box-shadow: 0 0 0 2px
-      ${props => (props.invert ? `rgba(0 0 0 0.5)` : `rgba(255 255 255 0.5)`)};
+      ${props => (props.invert ? `rgb(0 0 0 / 0.5)` : `rgb(255 255 255 / 0.5)`)};
   }
 
   @media screen and (max-width: 500px) {
@@ -30,7 +32,7 @@ const Link = styled.a`
 
 const BackLink = props => (
   <IsoLink href={props.absolute ? '/' : '..'}>
-    <Link>{BACK_ARROW} Back</Link>
+    <Link invert={props.invert}>{BACK_ARROW} Back</Link>
   </IsoLink>
 )
 
